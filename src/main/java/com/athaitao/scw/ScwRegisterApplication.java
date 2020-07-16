93452ae51de4730794518eb4986fb6cd09921616
“ze <1852749815@qq.com>
2020-07-16 16:39:23 +0800

    修改注释

--- src/main/java/com/athaitao/scw/ScwRegisterApplication.java
+++ src/main/java/com/athaitao/scw/ScwRegisterApplication.java
@@ -4,6 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/*
+ * 这是注册中心、
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
 
 @EnableEurekaServer
 @SpringBootApplication
